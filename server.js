520fac560686638589640d54d6f8e9012fc168e2
kesiahp18 <kesiahp18@gmail.com>
2021-03-14 16:08:13 -0400

    add indexedDB

--- server.js
+++ server.js
@@ -24,6 +24,8 @@ mongoose.connect(MONGODB_URI, {
 // routes
 app.use(require("./routes/api.js"));
 
+mongoose.set('debug', true);
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
